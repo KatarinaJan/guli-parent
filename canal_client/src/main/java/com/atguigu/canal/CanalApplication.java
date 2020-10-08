87ede016020ee09ff1eeaeca311f31cdd2f195c9
Jan <15779649834@163.com>
2020-10-08 14:06:16 +0800

    <新增>首次提交

---
+++ canal_client/src/main/java/com/atguigu/canal/CanalApplication.java
@@ -0,0 +1,31 @@
+package com.atguigu.canal;
+
+import com.atguigu.canal.client.CanalClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
+
+/**
+ * @Project: guli-parent
+ * @Describe: 描述
+ * @Author: Jan
+ * @Date: 2020-10-07 21:21
+ */
+@SpringBootApplication
+public class CanalApplication implements CommandLineRunner {
+
+    @Resource
+    private CanalClient canalClient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // 项目启动，执行canal客户端监听
+        canalClient.run();
+    }
+}
---
+++ infrastructure/api_gateway/src/main/java/com/atguigu/gateway/ApiGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.atguigu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Project: guli-parent
+ * @Describe: 描述
+ * @Author: Jan
+ * @Date: 2020-10-07 21:39
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
